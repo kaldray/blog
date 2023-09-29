9bdac8951a8e7695034cd01ed84ac25972e28718
kaldray <jordray@protonmail.com>
2023-09-29 19:06:13 +0200

    refactoring: hero component

--- apps/frontend/src/app/article/[uid]/page.tsx
+++ apps/frontend/src/app/article/[uid]/page.tsx
@@ -1,14 +1,12 @@
+import { Suspense } from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { redirect } from "next/navigation";
 
-import { ArticlesApiResponse } from "@app/types";
-
-import { Typographie } from "@app/Components/Typographie";
 import { css } from "@style-system/css";
-import { hstack, vstack } from "@style-system/patterns";
-import Image from "next/image";
-import Link from "next/link";
-import { Suspense } from "react";
+import { BackLink } from "@app/Components/BackLink";
+import { Hero } from "../../../Components/Hero";
+
+import type { ArticlesApiResponse } from "@app/types";
 
 export async function generateStaticParams() {
   const url = new URL(`${process.env.NEXT_PUBLIC_API_URL}/articles`);
@@ -50,87 +48,9 @@ export default async function Page({ params: { uid } }: PageProps) {
   }
   return (
     <>
-      <section
-        className={css({
-          bgColor: "primary",
-          w: "full",
-          h: "full",
-          display: "flex",
-          flexDir: "column",
-        })}>
-        <div
-          className={vstack({
-            w: "4/5",
-            mx: "auto",
-            alignItems: "flex-start",
-            h: "full",
-          })}>
-          <Link
-            className={css({
-              fontFamily: "montserrat",
-              fontSize: "1.125rem",
-              textDecoration: "underline",
-              _hover: {
-                scale: "1.1",
-              },
-              _active: {
-                scale: "1.1",
-              },
-            })}
-            href={"/"}>
-            Retour
-          </Link>
-          <div
-            className={hstack({
-              w: "full",
-              h: "full",
-              alignItems: "center",
-              justifyContent: "space-between",
-              gap: "1",
-              smDown: {
-                justifyContent: "space-around",
-                flexDir: "column",
-                alignItems: "center",
-              },
-            })}>
-            <div
-              className={vstack({
-                alignItems: "baseline",
-                justifyContent: "space-between",
-                gap: "1",
-              })}>
-              <Typographie
-                tag="h1"
-                className={css({
-                  letterSpacing: "wide",
-                  flex: 1,
-                })}>
-                {article?.attributes.title}
-              </Typographie>
-              <Typographie
-                tag="span"
-                className={css({
-                  bgColor: "accent",
-                  p: "1.5",
-                  px: "3",
-                  minW: "10",
-                  textAlign: "center",
-                  borderRadius: "sm",
-                })}>
-                {article?.attributes.categories.data.map((d) => {
-                  return "#" + d.attributes.category_name;
-                })}
-              </Typographie>
-            </div>
-            <Image
-              alt="todo"
-              src={`${article?.attributes.cover.data.attributes.url}`}
-              width={article?.attributes.cover.data.attributes.width}
-              height={article?.attributes.cover.data.attributes.height}
-            />
-          </div>
-        </div>
-      </section>
+      <Hero article={article} h1={article.attributes.title}>
+        <BackLink />
+      </Hero>
       <section className={css({ display: "grid", placeContent: "center", mt: "5" })}>
         <Suspense fallback={<p>loading</p>}>
           <MDXRemote source={article.attributes.content} />
